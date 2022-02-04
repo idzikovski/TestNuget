98848d91a9e8c799e0fec788114bed68cd464b7b
Ivan Djikovski <ivan.djikovski@wsa.com>
2022-02-04 14:39:03 +0100

    first

---
+++ TestNuget/TestClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace TestNuget
+{
+    public class TestClass
+    {
+        public TestClass()
+        {
+        }
+
+        public static int CalculateDifference(int a, int b)
+        {
+            int result = 0;
+
+            if (a > b)
+            {
+                result = a - b;
+            }
+
+            if (b >= a)
+            {
+                result = b - a;
+            }
+
+            return result;
+        }
+    }
+}
